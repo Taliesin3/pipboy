e1b5acda221b4fcaded600168c624262f438c07d
nathanhmiles <60706094+nathanhmiles@users.noreply.github.com>
2020-12-14 11:21:55 +0000

    Improved image positioning, edited bio

--- js/bio.js
+++ js/bio.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Type out bio 
-  const bio = 'I\'m a self-taught web developer, and am fluent in Mandarin Chinese.|I used to be a project manager working on criminal justice and legal development in China.|I decided to change careers when I finally realised I wanted to solve problems for a living.'
+  const bio = 'I\'m a self-taught web developer that for the past 10 years worked on, in and with China.|I\'m fluent in Mandarin Chinese, and until April 2020 I was a project manager working on criminal justice and legal development in China.|I decided to change careers when I finally realised I wanted to solve problems for a living.'
   const timePerLetter = 50;
   const newLineCharacter = '|';
 
